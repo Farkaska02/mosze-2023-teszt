9985dc6b6ea1de220aefa4a2ab211b1b66fc34de
Farkaska02 <farkas.mark.t@gmail.com>
2024-11-23 15:44:44 +0100

    hibák kijegyzetelése

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
+    int *b = new int[NELEMENTS];//elírás
     std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    for (int i = 0;)// felesleges ";"
     {
         b[i] = i * 2;
     }
     for (int i = 0; i; i++)
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:"//hiányzik a ";"
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
     for (int i = 0; i < N_ELEMENTS, i++)
     {
-        atlag += b[i]
+        atlag += b[i]//hiányzik a ";"
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
